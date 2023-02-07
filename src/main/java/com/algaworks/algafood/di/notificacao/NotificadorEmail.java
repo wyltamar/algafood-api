21c8561b3bf8819ccdaf0fa018a1586945977666
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2023-02-07 15:46:37 -0300

    Alterando a configuração do projeto dependendo do ambiente (com Spring Profiles)

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -1,28 +1,24 @@
 package com.algaworks.algafood.di.notificacao;
 
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.model.Cliente;
 
-@Profile("prod")
+
 @TipoDoNotificador(NivelUrgencia.NORMAL)
 @Component
 public class NotificadorEmail implements Notificador {
 	
-	@Value("${notificador.email.host-servidor}")
-	private String host;
-	
-	@Value("${notificador.email.porta-servidor}")
-	private Integer porta;
+	@Autowired
+	private NotificadorProperties properties;
 	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
 		
-		System.out.println("Host: " +host);
-		System.out.println("Porta: " + porta);
+		System.out.println("Host: " +properties.getHostServidor());
+		System.out.println("Porta: " + properties.getPortaServidor());
 				
 		System.out.printf("Notificando %s através do email %s : %s\n", 
 						cliente.getNome(),cliente.getEmail(),mensagem);
