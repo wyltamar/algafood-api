96cd5b03d302a7a9047d3cf5a0bf1cb463a496a8
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2023-02-09 19:32:40 -0300

    Exclusão dos exemplos anteriores/Preparando para iniciarmos com JPA

--- src/main/java/com/algaworks/algafood/di/notificacao/Notificador.java
+++
@@ -1,9 +0,0 @@
-package com.algaworks.algafood.di.notificacao;
-
-import com.algaworks.algafood.di.model.Cliente;
-
-public interface Notificador {
-
-	void notificar(Cliente cliente, String mensagem);
-
-}
