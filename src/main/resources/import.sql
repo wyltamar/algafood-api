7b19a89b9c2bed7476653cc8f3a400b172efd0d5
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2023-02-20 19:07:37 -0300

    Inserindo dados de restaurantes no banco de dados

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,5 @@
 insert into cozinha(nome) values ('Tailandesa');
 insert into cozinha(nome) values ('Indiana');
+insert into restaurante(nome,taxa_frete) values('Bom Gosto',10);
+insert into restaurante(nome,taxa_frete) values('Sabor da Terra',20);
+insert into restaurante(nome,taxa_frete) values('Point da Pizza',15);
