7a641ba4a62d3a02852506cf7c25aefdafbd1afe
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2023-02-24 19:43:00 -0300

    Criação do repositório de FormaPagamento

---
+++ src/main/java/com/algaworks/algafood/domain/repository/FormaPagamentoRepository.java
@@ -0,0 +1,20 @@
+package com.algaworks.algafood.domain.repository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.algaworks.algafood.domain.model.FormaPagamento;
+
+@Repository
+public interface FormaPagamentoRepository {
+	
+	public List<FormaPagamento> listar();
+	
+	public FormaPagamento buscar(Long id);
+	
+	public FormaPagamento salvar(FormaPagamento formaPagamento);
+	
+	public void remover(FormaPagamento formaPagamento);
+
+}
