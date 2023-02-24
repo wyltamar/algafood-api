722e44c0fed8d4a9e101ee689214d6996fb2f6aa
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2023-02-24 19:45:26 -0300

    Inserindo dados na tabela forma_pagamento através do arquivo import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,3 +3,5 @@ insert into cozinha(nome) values ('Indiana');
 insert into restaurante(nome,taxa_frete,cozinha_id) values('Bom Gosto',10,1);
 insert into restaurante(nome,taxa_frete,cozinha_id) values('Sabor da Terra',20,2);
 insert into restaurante(nome,taxa_frete,cozinha_id) values('Point da Pizza',15,1);
+insert into forma_pagamento(descricao,restaurante_id)values('cartão de crédito',1);
+insert into forma_pagamento(descricao,restaurante_id)values('pix',2);
