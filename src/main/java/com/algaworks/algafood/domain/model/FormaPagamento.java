ffe55e6e9a84027a9d6668af800257662222d6f4
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2023-02-24 18:37:21 -0300

    Criação da entidade FormaPagamento e mapeamento da classe

---
+++ src/main/java/com/algaworks/algafood/domain/model/FormaPagamento.java
@@ -0,0 +1,31 @@
+package com.algaworks.algafood.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class FormaPagamento {
+	
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String descricao;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Restaurante restaurante;
+
+}
