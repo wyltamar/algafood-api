ce15d80cd188d4d4934f9870d05485c9b3615d45
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2023-03-04 18:33:13 -0300

    Implementando content negotiation

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,7 @@ import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
 @RestController
-@RequestMapping(value = "/cozinhas")
+@RequestMapping(value = "/cozinhas")// produces = MediaType.APPLICATION_XML_VALUE)
 public class CozinhaController {
 	
 	@Autowired
