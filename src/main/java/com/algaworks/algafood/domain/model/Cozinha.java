7e3e7271b9003726f35231410c0487c6e2937200
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2023-03-06 21:58:53 -0300

    Classe cozinha atualizada

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -6,16 +6,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonRootName;
-
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-@JsonRootName("Gastronomia")
 public class Cozinha {
 	
 	@EqualsAndHashCode.Include
@@ -23,8 +18,7 @@ public class Cozinha {
 	@GeneratedValue(strategy = GenerationType.IDENTITY )
 	private Long id;
 	
-	//@JsonIgnore
-	@JsonProperty(value = "nome_cozinha")
+	
 	@Column(nullable = false)
 	private String nome;	
 
